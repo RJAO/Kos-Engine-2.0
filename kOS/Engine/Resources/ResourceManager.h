f9c44daef5c8a41ab9b83e54b56f05a1bc339fd8
Ng Jaz Winn <142562084+jazwinn@users.noreply.github.com>
2025-11-30 08:53:49 +0800

    Remove prefab resource type registration

--- kOS/Engine/Resources/ResourceManager.h
+++ kOS/Engine/Resources/ResourceManager.h
@@ -44,7 +44,6 @@ public:
 		RegisterResourceType<R_AudioStudio>(".bank");
 		RegisterResourceType<R_Material>(".mat");
 		RegisterResourceType<R_DepthMapCube>(".dcm");
-		RegisterResourceType<R_DepthMapCube>(".prefab");
 		RegisterResourceType<R_AnimController>(".controller");
     }
 
